ca59cfce6d4e75727dafef3b7b1fd8367481c8f1
Yang Guo <yangzong58@gmail.com>
2017-01-31 17:18:23 -0800

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require("express");
 var app = express();
+var mongoose = require('mongoose');
+// mlab login:centralis password:centaur1
+mongoose.connect('mongodb://user:password@ds129459.mlab.com:29459/centralis');
+
+// // Sample model
+// var yang = new User({
+//   name: 'Yang',
+//   role: 'waterboy',
+//   room: 445
+// });
+// yang.save(function(err) {
+//   if (err) throw err;
+//   console.log('User saved successfully!');
+// });
 
 // routes ========================================
-require('./app/routes.js')(app); 
+require('./app/routes.js')(app);
