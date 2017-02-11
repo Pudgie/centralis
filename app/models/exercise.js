8e7212b43422c8242dd8b91019b7b94c7d13e837
Jessie Guo <yangzigu@usc.edu>
2017-02-11 12:40:31 -0800

    exercise model

---
+++ app/models/exercise.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+var exerciseSchema = new Schema({
+	id: Number,
+  	name: String,
+  	scenarios: [{type: Schema.ObjectId, ref: 'Scenario'}]
+});
+
+var scenarioSchema = new Schema({
+	videoURL: String,
+  	text: String,
+  	question: String,
+  	survey: {type: Schema.ObjectId, ref: 'Survey'}
+});
+
+var surveySchema = new Schema({
+	surveyQuestions: [String]
+});
+
+var Exercise = mongoose.model('Exercise', exerciseSchema);
+var Scenario = mongoose.model('Scenario', scenarioSchema);
+var Survey = mongoose.model('Survey', surveySchema);
+
+module.exports = Exercise;
+module.exports = Scenario;
+module.exports = Survey;
