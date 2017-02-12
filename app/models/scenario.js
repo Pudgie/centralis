2d9487c7afed4485eadf6c7768c676e0a9a46324
Yang Guo <yangzong58@gmail.com>
2017-02-12 14:05:58 -0800

    survey and scenario forms

--- app/models/scenario.js
+++ app/models/scenario.js
@@ -2,12 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 var Survey = require('./survey');
+var autoIncrement = require('mongoose-auto-increment');
 
 var scenarioSchema = new Schema({
 	videoURL: String,
 	text: String,
 	question: String,
 	survey: {type: Schema.ObjectId, ref: 'Survey'}
+});
+
+scenarioSchema.plugin(autoIncrement.plugin, {
+	model: 'Scenario',
+	startAt: 1,
+  incrementBy: 1
 });
 
 var Scenario = mongoose.model('Scenario', scenarioSchema);
