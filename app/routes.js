3851ae544d3cae3b12581888822362dc0acb83ff
Fred Lin's MacBook Pro <linfengc@usc.edu>
2017-03-19 22:15:22 -0700

    reorganized css to a single sheet. made design consistent

--- app/routes.js
+++ app/routes.js
@@ -82,7 +82,7 @@ module.exports = function(app, passport) {
 				roomNumber: rooms[i],
 				activeSessionID: sessionID,
 				nextScenario: 1
-				exerciseID: req.body.exerciseID
+				//exerciseID: req.body.exerciseID
 			});
 			session.save(function(err) {
 				if (err) { throw err; }
