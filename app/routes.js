3fd9f0f9792d731b664edc92e3574c90b5d48eb9
Jessie Guo <yangzigu@usc.edu>
2017-04-04 12:11:41 -0700

    removed some console logs and duplicate /display

--- app/routes.js
+++ app/routes.js
@@ -108,21 +108,6 @@ module.exports = function(app, passport) {
 				res.redirect('/assignDisruption?sessionID='+sessionID, {currRound: currRound});
 			}
 		}
-		// for (var ii = 0; ii < rooms.length; ii++) {
-		// 	if (disruptionSelection[ii] != null && disruptionSelection[ii] != -1) {
-		// 		// BUG: both async calls are fucking things up
-		// 		Session.findOneAndUpdate({roomNumber: String(rooms[ii]), activeSessionID: sessionID, currRound: 1},
-		// 			{$set: {'nextScenario': disruptionSelection[ii]},
-		// 			$inc: {'currRound': 1}},
-		// 			function(err, model) {
-		// 				if (err) throw err;
-		// 			}
-		// 		);
-		// 	}
-		// 	else { // at least one scenario is not selected
-		// 		res.redirect('/assignDisruption?sessionID='+sessionID, {currRound: currRound});
-		// 	}
-		// }
 		currRound++;
 		res.redirect('/adminWait?sessionID='+sessionID+"&currRound="+currRound);
 	});
@@ -271,119 +256,7 @@ module.exports = function(app, passport) {
 			res.render('selectExercise.ejs', {exercises: results});
 		});
 	});
-	app.post('/display', function(req, res) {
-		var role = req.body.roles;
-		var room = req.body.room;
-		var sid = req.body.studentID;
-		var sess = req.body.sessionID;
-		console.log("DISPLAY ROLE:" + role);
-		Session.findOne({'roomNumber': room, 'activeSessionID' : sess}).lean().exec( function(err, result) {
-			//get the session
-			var id = result.exerciseID; //pull out exercise ID for that session
-			var currentRound = result.currRound;
-			var next;
-			var isDone = false;
-			var students = result.students;
-			for (var i = 0; i < students.length; i++) {
-				if (students[i].id == sid) {
-					next = students[i].nextScenario;
-					isDone = students[i].firstRoundDone;
-					break;
-				}
-			}
 
-			console.log("EXERCISEID:" + id);
-			console.log("CURRENTROUND:" + currentRound);
-			Exercise.findOne({'_id': id}).lean().exec( function(err, exercise) {
-				if (err) throw err;
-				//find session ID;
-				if (currentRound > exercise.numOfRounds + 1) {
-					// render finish page
-					console.log("finished exercise");
-					res.render('finish.ejs');
-					return;
-				} else {
-					// check if can proceed
-					if (result.nextScenario != null && result.nextScenario != 0 && !isDone) {
-						next = result.nextScenario;
-						Session.findOneAndUpdate(
-							{"roomNumber": room, "activeSessionID" : sess, "students.id": parseInt(sid)},
-							{$set: {"students.$.firstRoundDone": true}},
-							{new: true},
-							function(err, result) {
-								if (err) throw err;
-								console.log("Set firstRoundDone to true successfully");
-							}
-						);
-					}
-					if (next == null && currentRound == exercise.numOfRounds + 1) {
-						res.render('finish.ejs');
-						return;
-					}
-					if (next == null) {
-						res.render('survey2.ejs', {role: role, room: room, message: 'Please wait until admin chooses next scenario', url: "", sessionID: sess, studentID: sid});
-						return;
-					}
-
-					if (next == 0) {
-						res.render('survey2.ejs', {role: role, room: room, message: 'Please wait until admin chooses the first scenario', url: "", sessionID: sess, studentID: sid});
-						return;
-					}
-
-					//set nextScenario of studentID to NULL
-					Session.findOneAndUpdate(
-						{"roomNumber": room, "activeSessionID" : sess, "students.id": parseInt(sid)},
-						{$set: {"students.$.nextScenario": null}},
-						{new: true},
-						function(err, model) {
-							if (err) throw err;
-							console.log("Set nextScenario of studentID to null successfully");
-						}
-					);
-
-					if (currentRound == 2) {
-						for (var i = 0; i < exercise.scenarios.length; i++) {
-		 					if (exercise.scenarios[i].round == 1){
-		 						var results = exercise.scenarios[i];
-								if (results.videoURL == null) {
-									res.render('scenario.ejs', {text: results.text, role: role, room: room, sessionID: sess, studentID: sid});
-			 						return;
-								} else {
-									var id = results.videoURL.split("?v=");
-									console.log("asdfads");
-									console.log(id);
-									res.render('video.ejs', {id: id[1], role: role, room: room, sessionID: sess, studentID: sid});
-								}
-		 					}
-	 					}
-					}
-					else if (currentRound > 2) {
-						console.log("FLAG1");
-						for (var i = 0; i < exercise.scenarios.length; i++) {
-							console.log("i:" + i + " length:" + exercise.scenarios.length);
-							console.log("currRound:" + currentRound -1);
-							console.log("sc ID: " + exercise.scenarios[i].id + " next:" + next);
-		 					if (exercise.scenarios[i].round == currentRound - 1 && exercise.scenarios[i].id == next){
-		 						console.log("FLAG2");
-		 						var results = exercise.scenarios[i];
-								if (results.videoURL == null) {
-									res.render('scenario.ejs', {text: results.text, role: role, room: room, sessionID: sess, studentID: sid});
-			 						return;
-								} else {
-									var id = results.videoURL.split("?v=");
-									console.log("asdfads");
-									console.log(id);
-									res.render('video.ejs', {id: id[1], role: role, room: room, sessionID: sess, studentID: sid});
-									return;
-								}
-		 					}
-	 					}
-					}
-					console.log("line 347 in /display error: possibly undefined var results");
-				 }
-			});
-		});
-	});
 	app.post('/display', function(req, res) {
 		var role = req.body.roles;
 		var room = req.body.room;
@@ -395,7 +268,6 @@ module.exports = function(app, passport) {
 			var currentRound = result.currRound;
 			var next;
 			var students = result.students;
-			console.log("sid:" + sid);
 			for (var i = 0; i < students.length; i++) {
 				if (students[i].id == sid) {
 					console.log("students[i].id:" + students[i].id);
@@ -404,8 +276,6 @@ module.exports = function(app, passport) {
 				}
 			}
 
-			console.log("nextScenario:" + next);
-			console.log("CURRENTROUND:" + currentRound);
 			Exercise.findOne({'_id': id}).lean().exec( function(err, exercise) {
 				if (err) throw err;
 				//find session ID;
@@ -584,7 +454,6 @@ module.exports = function(app, passport) {
 	// });
 
 	app.post('/reset', function(req, res) {
-		sCount = 0;
 		res.redirect('/logout');
 	});
 
